1d19211a974395b25d6cb2216681a4e2d96a0254
Jozef <balunjozef@gmail.com>
2016-04-22 15:19:43 +0200

    Update flash.blade.php

--- src/views/flash.blade.php
+++ src/views/flash.blade.php
@@ -1,7 +1,7 @@
 @if(Session::has('flash.alerts'))
     @foreach(Session::get('flash.alerts') as $alert)
 
-        <div class='alert alert-{{ $alert['level'] }} @if(alert['important'] ? alert-important : '' @endif)'>
+        <div class='alert alert-{{ $alert['level'] }} @if($alert['important']) alert-important @endif'>
             <button class="close" type="button" data-dismiss="alert" aria-hidden="true">&times;</button>
 
             @if( ! empty($alert['title']))
